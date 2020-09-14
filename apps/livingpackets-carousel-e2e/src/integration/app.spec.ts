80bf2f2380ed2cc7c94b48142814175c10649ea9
Tong Sheng Ng <tongsheng.ng@cheetahdigital.com>
2020-09-14 14:11:33 +0800

    renamed to livingpackets-carousel

--- apps/livingpackets-carousel-e2e/src/integration/app.spec.ts
+++ apps/livingpackets-carousel-e2e/src/integration/app.spec.ts
@@ -1,6 +1,6 @@
 import { getGreeting } from '../support/app.po';
 
-describe('livingpackets', () => {
+describe('livingpackets-carousel', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
@@ -8,6 +8,6 @@ describe('livingpackets', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to livingpackets!');
+    getGreeting().contains('Welcome to livingpackets-carousel!');
   });
 });
--- apps/livingpackets-carousel/jest.config.js
+++ apps/livingpackets-carousel/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'livingpackets',
+  name: 'livingpackets-carousel',
   preset: '../../jest.config.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
@@ -9,5 +9,5 @@ module.exports = {
     ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
-  coverageDirectory: '../../coverage/apps/livingpackets',
+  coverageDirectory: '../../coverage/apps/livingpackets-carousel',
 };
